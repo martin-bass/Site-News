53b38e2eb4fb0bd9ea1daa28e5d3c9eed8b4a01a
Martin <martin_reym@hotmail.com>
2021-09-18 13:57:25 -0300

    first commit

---
+++ Main.js
@@ -0,0 +1,10 @@
+document.querySelector (".menu-btn").addEventListener ('click', ()=> {
+    document.querySelector(".nav-menu").classList.toggle("show");
+});
+
+ScrollReveal().reveal('.Showcase', {delay: 550});
+ScrollReveal().reveal('.news-cards', {delay: 550});
+ScrollReveal().reveal('.banner-1', {delay: 550});
+ScrollReveal().reveal('.banner-2', {delay: 550});
+
+
